4d02b7d7d886b50169c49ff47b7ecb352d872d53
Andre Oliveira da Silva <Andre Oliveira da Silva>
2017-06-16 00:10:47 -0300

    inclusao do arquivo batch

---
+++ callAPI.bat
@@ -0,0 +1,37 @@
+@echo off
+:start
+CLS
+echo SELECIONE A SUA OPCAO:
+echo 1 - Lista dos 5 usuarios com mais seguidores
+echo 2 - Lista com o total de tweets em portugues para cada #tag
+echo 3 - Lista com o total de tweets por hora do dia
+echo 9 - Encerrar
+
+SET /P _option=
+IF "%_option%"=="1" GOTO :topUsers 
+
+IF "%_option%"=="2" GOTO :totalPT
+
+IF "%_option%"=="3" GOTO :totalHours
+
+IF "%_option%"=="9" GOTO :end
+
+echo Selecione uma opcao entre 1 e 4
+PAUSE
+CLS
+GOTO:start
+
+:topUsers
+start "" http://localhost:3000/topUsers
+GOTO :start
+
+:totalPT
+start "" http://localhost:3000/totalPT
+GOTO :start
+
+:totalHours
+start "" http://localhost:3000/totalHours
+GOTO :start
+
+:end
+exit
